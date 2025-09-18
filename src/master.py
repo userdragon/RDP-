e27ea5af94d5577804ba7ccac6336c000ab6e59b
userdragon <73782254+userdragon@users.noreply.github.com>
2025-09-18 15:22:36 +0800

    Create master.py

---
+++ src/master.py
@@ -0,0 +1,18 @@
+# src/master.py
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
+from core.common.logger import logger
+
+class MasterWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("同步远程控制 - 主控端")
+        self.setGeometry(100, 100, 800, 600)
+        self.label = QLabel("主控端运行中...", self)
+        self.label.setGeometry(50, 50, 300, 30)
+
+if __name__ == "__main__":
+    logger.info("主控端启动")
+    app = QApplication([])
+    window = MasterWindow()
+    window.show()
+    app.exec_()
